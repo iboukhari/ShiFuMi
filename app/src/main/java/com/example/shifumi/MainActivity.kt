c9d91036f540f854aeda8cd8e5fe23fb3a35d573
Ilyas Boukhari <ilyasboukh87@gmail.com>
2025-02-22 18:45:49 +0100

    Ajout de la détection des secousses avec le gyroscope

--- app/src/main/java/com/example/shifumi/MainActivity.kt
+++ app/src/main/java/com/example/shifumi/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.shifumi
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,9 +23,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -91,14 +104,84 @@ fun HomeScreen(navController: NavHostController) {
 
 @Composable
 fun PlayScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    val sensorManager = remember { context.getSystemService(SensorManager::class.java) }
+    val accelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    var shakeCount by remember { mutableStateOf(0) }
+    var lastShakeTime by remember { mutableStateOf(0L) }
+    var selectedWeapon by remember { mutableStateOf<Int?>(null) }
+
+
+
+
+    DisposableEffect (sensorManager) {
+        val sensorListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                event?.let {
+                    val x = it.values[0]
+                    val y = it.values[1]
+                    val z = it.values[2]
+
+                    val acceleration = Math.sqrt((x * x + y * y + z * z).toDouble())
+                    val shakeThreshold = 14
+                    val currentTime = System.currentTimeMillis()
+
+                    if (acceleration > shakeThreshold) {
+                        if (currentTime - lastShakeTime > 400) {
+                            lastShakeTime = currentTime
+                            shakeCount++
+
+                            Log.d("Sensor", "Secousse détectée ! Nombre : $shakeCount")
+
+                            if (shakeCount >= 3) {
+                                shakeCount = 0
+
+                                
+                                val weapons = listOf(R.drawable.pierre, R.drawable.papier, R.drawable.ciseaux)
+                                selectedWeapon = weapons.random()
+
+                                Log.d("Sensor", "Arme sélectionnée : $selectedWeapon")
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+        }
+
+        accelerometer?.let {
+            sensorManager.registerListener(sensorListener, it, SensorManager.SENSOR_DELAY_UI)
+        }
+
+        onDispose {
+            Log.d("Sensor", "Capteur désenregistré")
+            sensorManager.unregisterListener(sensorListener)
+        }
+    }
+
+
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize().padding(16.dp).padding(top = 40.dp),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(text = "Ecran de jeu",
             style = MaterialTheme.typography.titleMedium)
+
+        selectedWeapon?.let { weapon ->
+            Image(
+                painter = painterResource(id = weapon),
+                contentDescription = "Arme sélectionnée",
+                modifier = Modifier.fillMaxSize()
+                    .padding(top = 20.dp)
+                    .height(150.dp)
+                    .animateContentSize()
+            )
+        }
     }
+
 }
 
 @Composable
